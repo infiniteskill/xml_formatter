cf4ca26e4544c1a3f79fddecc8fda47e0499af26
infiniteskill <rajneeshverma55@gmail.com>
2020-03-15 12:24:47 +0530

    upload

---
+++ Readme.txt
@@ -0,0 +1,5 @@
+1. Usage
+
+    Put any unformatted xml data in unformatted.xml and save it.
+    run using "python formatter.py" in anaconda or normal python 3 environment.
+    formatted xml will be displayed in console.
---
+++ formatter.py
@@ -0,0 +1,24 @@
+from xml.etree import ElementTree
+
+def indent(elem, level=0):
+    i = "\n" + level*"  "
+    j = "\n" + (level-1)*"  "
+    if len(elem):
+        if not elem.text or not elem.text.strip():
+            elem.text = i + "  "
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+        for subelem in elem:
+            indent(subelem, level+1)
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = j
+    else:
+        if level and (not elem.tail or not elem.tail.strip()):
+            elem.tail = j
+    return elem        
+
+root = ElementTree.parse('unformatted.xml').getroot()
+tree=indent(root)
+print("Formatted xml is: \n")
+ElementTree.dump(tree)
+
